cdd9d15a70328830fccd665a240ca09f7ec3f20b
Nick Kearns <nickkearns133@gmail.com>
2023-01-18 12:34:07 -0500

    init

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Nick Kearns on 1/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
